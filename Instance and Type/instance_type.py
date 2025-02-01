252d36cdd73040a1e07b3b409087948b4dbae72c
tamil <tamil.accenture@gmail.com>
2025-02-01 15:31:54 -0500

    Instance&Type

---
+++ Instance and Type/instance_type.py
@@ -0,0 +1,18 @@
+class Zoology:
+    def __init__ (self, definition):
+        self.definition = definition
+
+class Botany:
+    def __init__(self, definition):
+        self.definition = definition
+
+z1 = Zoology("Study of Animals")
+b1 = Botany("Study of Plants")
+
+print(z1.definition)
+print(b1.definition)
+
+print(f"The type of b1 is {type(b1)} and type of z1 is {type(z1)}")
+print(type(b1) == type(z1))
+print(isinstance(b1,Zoology))
+print(isinstance(b1,Botany))
