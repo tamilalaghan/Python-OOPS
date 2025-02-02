1b6c96cc0e4b597f9d8f93d3108a9905539947ba
tamil <tamil.accenture@gmail.com>
2025-02-02 16:56:37 -0500

    Multiple Inheritance Execute Three

--- Muliple Inheritance/README.md
+++ Muliple Inheritance/README.md
@@ -47,7 +47,7 @@ class Hydrophyte:
         #The use of Super is essential
         super().__init__()
         self.habitat = "Aquatic or Water living"
-        self.diversity_range = "Aquatic plants only"
+        self.diveristy_range = "Aquatic plants only"
 
 
 class Lily(PlantKingdom,Hydrophyte):
@@ -68,3 +68,42 @@ print(Lily.__mro__)
 
 Outcome
 ![alt text](image-1.png)
+
+
+Execute Three
+
+```py
+# Execute Three\
+class PlantKingdom:
+    def __init__(self):
+        #The use of Super is essential
+        super().__init__()
+        self.species_kingdom = "Plant Kindom"
+        self.diversity_range = "Large Range of Plants"
+
+class Hydrophyte:
+    def __init__(self):
+        #The use of Super is essential
+        super().__init__()
+        self.habitat = "Aquatic or Water living"
+        self.diversity_range = "Aquatic plants only"
+
+#Notice Change of Order in arguments
+class Lily(Hydrophyte,PlantKingdom):
+    def __init__(self):
+        super().__init__()
+    
+    def printprops(self):
+        print(f"Kingdom : {self.species_kingdom}")
+        print(f"Habitat : {self.habitat}")
+        print(f"Diversity : {self.diversity_range}")
+        return
+
+lil = Lily()
+lil.printprops()
+# Method Resolution Order
+print(Lily.__mro__)
+```
+
+Outcome
+![alt text](image-2.png)
--- Muliple Inheritance/mulipleinheritance.py
+++ Muliple Inheritance/mulipleinheritance.py
@@ -24,13 +24,44 @@
 # lil = Lily()
 # lil.printprops()
 
-# Execute Two\
+# # Execute Two\
+# class PlantKingdom:
+#     def __init__(self):
+#         #The use of Super is essential
+#         super().__init__()
+#         self.species_kingdom = "Plant Kindom"
+#         self.diveristy_range = "Large Range of Plants"
+
+# class Hydrophyte:
+#     def __init__(self):
+#         #The use of Super is essential
+#         super().__init__()
+#         self.habitat = "Aquatic or Water living"
+#         self.diveristy_range = "Aquatic plants only"
+
+
+# class Lily(PlantKingdom,Hydrophyte):
+#     def __init__(self):
+#         super().__init__()
+    
+#     def printprops(self):
+#         print(f"Kingdom : {self.species_kingdom}")
+#         print(f"Habitat : {self.habitat}")
+#         print(f"Diversity : {self.diveristy_range}")
+#         return
+
+# lil = Lily()
+# lil.printprops()
+# # Method Resolution Order
+# print(Lily.__mro__)
+
+# Execute Three\
 class PlantKingdom:
     def __init__(self):
         #The use of Super is essential
         super().__init__()
         self.species_kingdom = "Plant Kindom"
-        self.diveristy_range = "Large Range of Plants"
+        self.diversity_range = "Large Range of Plants"
 
 class Hydrophyte:
     def __init__(self):
@@ -39,15 +70,15 @@ class Hydrophyte:
         self.habitat = "Aquatic or Water living"
         self.diversity_range = "Aquatic plants only"
 
-
-class Lily(PlantKingdom,Hydrophyte):
+#Notice Change of Order in arguments
+class Lily(Hydrophyte,PlantKingdom):
     def __init__(self):
         super().__init__()
     
     def printprops(self):
         print(f"Kingdom : {self.species_kingdom}")
         print(f"Habitat : {self.habitat}")
-        print(f"Diversity : {self.diveristy_range}")
+        print(f"Diversity : {self.diversity_range}")
         return
 
 lil = Lily()
