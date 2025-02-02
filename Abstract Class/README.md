fe8d11c850dcb23fc2ca6c0829a342bb0ee0b891
tamil <tamil.accenture@gmail.com>
2025-02-02 15:58:40 -0500

    Execute Two

--- Abstract Class/README.md
+++ Abstract Class/README.md
@@ -38,3 +38,41 @@ ortax = OldRegime()
 
 Outcome
 ![alt text](image.png)
+
+
+Execute Two
+
+```py
+#Execute Two
+from abc import ABC, abstractmethod
+
+class Tax(ABC):
+    def __init__(self):
+        super().__init__()
+    
+    @abstractmethod
+    def taxregime(self):
+        pass
+
+
+class NewRegime(Tax):
+    def __init__(self):
+        super().__init__()
+    
+    def taxregime(self,netincome):
+        maxtaxation = 0.3*netincome
+        print(f"The max taxation is {maxtaxation}")
+        return 
+
+class OldRegime(Tax):
+    def __init__(self):
+        super().__init__()
+    
+    
+# taxabc = Tax()
+nrtax = NewRegime()
+nrtax.taxregime(50000)
+ortax = OldRegime()
+```
+Outcome
+![alt text](image-1.png)
--- Abstract Class/abstract.py
+++ Abstract Class/abstract.py
@@ -1,5 +1,37 @@
 
-#Execute One
+# #Execute One
+# from abc import ABC, abstractmethod
+
+# class Tax(ABC):
+#     def __init__(self):
+#         super().__init__()
+    
+#     @abstractmethod
+#     def taxregime(self):
+#         pass
+
+
+# class NewRegime(Tax):
+#     def __init__(self):
+#         super().__init__()
+    
+#     def taxregime(self,netincome):
+#         maxtaxation = 0.3*netincome
+#         print(f"The max taxation is {maxtaxation}")
+#         return 
+
+# class OldRegime(Tax):
+#     def __init__(self):
+#         super().__init__()
+    
+    
+# taxabc = Tax()
+# nrtax = NewRegime()
+# nrtax.taxregime(50000)
+# ortax = OldRegime()
+
+
+#Execute Two
 from abc import ABC, abstractmethod
 
 class Tax(ABC):
@@ -25,7 +57,7 @@ class OldRegime(Tax):
         super().__init__()
     
     
-taxabc = Tax()
+# taxabc = Tax()
 nrtax = NewRegime()
 nrtax.taxregime(50000)
 ortax = OldRegime()
