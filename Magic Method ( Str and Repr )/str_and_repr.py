3c513b16e795db2390cdc359eab76f211980072a
tamil <tamil.accenture@gmail.com>
2025-02-03 19:35:16 -0500

    Magic Method (Str and Repr) Execute One

---
+++ Magic Method ( Str and Repr )/str_and_repr.py
@@ -0,0 +1,14 @@
+# Execute One
+class Cellphone:
+    def __init__(self,brand,model,os):
+        self.brand = brand
+        self.model = model
+        self.os = os
+
+
+apple = Cellphone("Apple","15Pro","ios")
+samsung = Cellphone("Samsung","Galaxy S25 Ultra","android")
+
+
+print(apple)
+print(samsung)
