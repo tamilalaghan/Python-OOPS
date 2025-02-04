3d2639f25d11a5a30f8473f6932b3ec57bb80418
tamil <tamil.accenture@gmail.com>
2025-02-04 18:22:18 -0500

    Magic Method ( getattribute and getattr ) Execute One RM

--- Magic Method ( getattribute and getattr )/getattribute_getattr.py
+++ Magic Method ( getattribute and getattr )/getattribute_getattr.py
@@ -1,4 +1,3 @@
-
 # Execute one
 class Cellphone:
     def __init__(self,brand,model,os,price_usd):
@@ -10,12 +9,21 @@ class Cellphone:
     def __str__(self):
         return f"{self.brand}'s {self.model}, runs on {self.os} os and is priced at ${self.price_usd}"
     
-    
 nokia = Cellphone("Nokia","1100","symbian",150)
 samsung = Cellphone("Samsung","Galaxy S25 Ultra","andriod",1900)
 apple = Cellphone("Apple","15 ProMax","ios",2100)
 
-
 print(nokia)
 print(samsung)
 print(apple)
+
+
+
+
+
+
+
+    # def __getattribute__(self, name):
+    #     if name == "price_usd":
+    #         return super().__getattribute__("price_usd") * 0.75
+    #     return super().__getattribute__(name)
