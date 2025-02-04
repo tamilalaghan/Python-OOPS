c777780fcc0b97802148ec8a7b05021bad5c1049
tamil <tamil.accenture@gmail.com>
2025-02-03 20:58:51 -0500

    Magic Method ( Str and Repr ) Execute Three

--- Magic Method ( Str and Repr )/README.md
+++ Magic Method ( Str and Repr )/README.md
@@ -44,3 +44,32 @@ print(str(apple))
 print(repr(samsung))
 ```
 ![alt text](image-1.png)
+
+Execute Three
+
+```py
+# Execute Three
+class Cellphone:
+    def __init__(self,brand,model,os):
+        self.brand = brand
+        self.model = model
+        self.os = os
+    
+    def __str__(self):
+        return f"{self.brand}'s {self.model} runs on {self.os}"
+   
+    def __repr__(self):
+        return f"Brand : {self.brand}, Model : {self.model}, OS : {self.os}"
+
+class Laptop:
+    def __init__(self,brand,model,os):
+        self.brand = brand
+        self.model = model
+        self.os = os
+
+predator = Laptop("Acer","Triton 18","windows")
+
+apple = Cellphone("Apple","15Pro","ios")
+samsung = Cellphone("Samsung","Galaxy S25 Ultra","android")
+```
+![alt text](image-2.png)
--- Magic Method ( Str and Repr )/str_and_repr.py
+++ Magic Method ( Str and Repr )/str_and_repr.py
@@ -14,7 +14,27 @@
 # print(apple)
 # print(samsung)
 
-# Execute Two
+# # Execute Two
+# class Cellphone:
+#     def __init__(self,brand,model,os):
+#         self.brand = brand
+#         self.model = model
+#         self.os = os
+    
+#     def __str__(self):
+#         return f"{self.brand}'s {self.model} runs on {self.os}"
+   
+#     def __repr__(self):
+#         return f"Brand : {self.brand}, Model : {self.model}, OS : {self.os}"
+
+# apple = Cellphone("Apple","15Pro","ios")
+# samsung = Cellphone("Samsung","Galaxy S25 Ultra","android")
+
+
+# print(str(apple))
+# print(repr(samsung))
+
+# Execute Three
 class Cellphone:
     def __init__(self,brand,model,os):
         self.brand = brand
@@ -27,10 +47,18 @@ class Cellphone:
     def __repr__(self):
         return f"Brand : {self.brand}, Model : {self.model}, OS : {self.os}"
 
+class Laptop:
+    def __init__(self,brand,model,os):
+        self.brand = brand
+        self.model = model
+        self.os = os
+
+predator = Laptop("Acer","Triton 18","windows")
 
 apple = Cellphone("Apple","15Pro","ios")
 samsung = Cellphone("Samsung","Galaxy S25 Ultra","android")
 
 
 print(str(apple))
+print(str(predator))
 print(repr(samsung))
