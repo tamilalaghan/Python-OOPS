6cd7f73ac2a294df014a5b53d0abf4a7a2d1ea5e
tamil <tamil.accenture@gmail.com>
2025-02-03 20:15:44 -0500

    Magic Method ( Str and Repr ) Execute Two

--- Magic Method ( Str and Repr )/README.md
+++ Magic Method ( Str and Repr )/README.md
@@ -18,3 +18,29 @@ print(samsung)
 ```
 Outcome
 ![alt text](image.png)
+
+Execute Two
+
+```py
+# Execute Two
+class Cellphone:
+    def __init__(self,brand,model,os):
+        self.brand = brand
+        self.model = model
+        self.os = os
+    
+    def __str__(self):
+        return f"{self.brand}'s {self.model} runs on {self.os}"
+   
+    def __repr__(self):
+        return f"Brand : {self.brand}, Model : {self.model}, OS : {self.os}"
+
+
+apple = Cellphone("Apple","15Pro","ios")
+samsung = Cellphone("Samsung","Galaxy S25 Ultra","android")
+
+
+print(str(apple))
+print(repr(samsung))
+```
+![alt text](image-1.png)
--- Magic Method ( Str and Repr )/str_and_repr.py
+++ Magic Method ( Str and Repr )/str_and_repr.py
@@ -1,14 +1,36 @@
-# Execute One
+# # Execute One
+# class Cellphone:
+#     def __init__(self,brand,model,os):
+#         self.brand = brand
+#         self.model = model
+#         self.os = os
+
+
+
+# apple = Cellphone("Apple","15Pro","ios")
+# samsung = Cellphone("Samsung","Galaxy S25 Ultra","android")
+
+
+# print(apple)
+# print(samsung)
+
+# Execute Two
 class Cellphone:
     def __init__(self,brand,model,os):
         self.brand = brand
         self.model = model
         self.os = os
+    
+    def __str__(self):
+        return f"{self.brand}'s {self.model} runs on {self.os}"
+   
+    def __repr__(self):
+        return f"Brand : {self.brand}, Model : {self.model}, OS : {self.os}"
 
 
 apple = Cellphone("Apple","15Pro","ios")
 samsung = Cellphone("Samsung","Galaxy S25 Ultra","android")
 
 
-print(apple)
-print(samsung)
+print(str(apple))
+print(repr(samsung))
